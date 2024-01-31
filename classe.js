210ed1acefe4dc219a1d7d0e8998decc3c8ba1ed
camila-OL <camila-OL>
2024-01-31 20:38:32 -0300

    ataque

--- classe.js
+++ classe.js
@@ -72,18 +72,34 @@ class Stage {
     }
     update() {
         //fighter 1
-        this.fighterEl1.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP` 
+        this.fighterEl1.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP` 
         let f1pct = (this.fighter1.life / this.fighter1.maxLife) * 100
         this.fighterEl1.querySelector('.bar').style.width = `${f1pct}%`
 
         //fighter 2
-        this.fighterEl2.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`
+        this.fighterEl2.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`
         let f2pct = (this.fighter2.life / this.fighter2.maxLife) * 100
         this.fighterEl2.querySelector('.bar').style.width = `${f2pct}%`
     }
 
     doAttack(attacking, attacked) {
-        console.log(`${attacking.name} está atacando ${attacked.name}`)
+        if(attacking.life <= 0 || attacked.life <= 0) {
+            console.log('já morreu')
+            return
+        }
+
+        let attackFactor = (Math.random() * 2).toFixed(2)
+        let defenceFactor = (Math.random() * 2).toFixed(2)
+
+        let actualAttack = attacking.attack * attackFactor
+        let actualDefence = attacked.defence * defenceFactor
+        
+        if(actualAttack > actualDefence) {
+            attacked.life -= actualAttack
+            console.log(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`)
+        } else {
+            console.log(`${attacked.name} conseguiu se defender`)
+        }
     
         this.update()
     }
